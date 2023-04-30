8d4356169cad8cc7ef4fe7b18446c60a99747cc1
ismlslckk <ismlslck@gmail.com>
2023-04-30 14:03:40 +0200

    signup page is created

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,11 @@ import ReactDOM from 'react-dom/client';
 import 'antd/dist/reset.css';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import rootReducer from './store';
 import { Provider } from 'react-redux';
-
-
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
@@ -16,6 +15,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
